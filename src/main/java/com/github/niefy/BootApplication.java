faa938a70910a5cd3544c25da12a78e3c5c952e8
rjfu <1004074162@qq.com>
2020-11-14 23:23:31 +0800

    测试

--- src/main/java/com/github/niefy/BootApplication.java
+++ src/main/java/com/github/niefy/BootApplication.java
@@ -12,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class BootApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(BootApplication.class, args);
     }
 
